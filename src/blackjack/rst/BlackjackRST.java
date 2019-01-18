c60d13b241e2a5af51b28478ac87684bb428a5dd
toom5002 <45435322+toom5002@users.noreply.github.com>
2019-01-18 14:04:51 -0500

    Finished code without GUI

--- src/blackjack/rst/BlackjackRST.java
+++ src/blackjack/rst/BlackjackRST.java
@@ -3,8 +3,3 @@ public class BlackjackRST {
     public static void main(String[] args) throws InterruptedException {
         DeckOfCards player = new DeckOfCards();
     }}
-
-
-// have it exit all the the checkWL after it procks once
-// then make sure it loops properly and have it check loopholes and things that may crash
-// check to make sure it runs smoothly through classes
--- src/blackjack/rst/DeckOfCards.java
+++ src/blackjack/rst/DeckOfCards.java
@@ -5,16 +5,16 @@ import java.util.Random;
 import javax.swing.*;
 
 public class DeckOfCards {
-    
-    //this is a comment
 
+    //this is a comment
+    String[] buttonRP = {"Keep playing", "Cash Out"};
     String[] buttons = {"Hit", "Stand"};
     String[] suit = {"Clubs", "Diamonds", "Hearts", "Spades"};
     String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
     String deck[][] = new String[13][4];
     String deckTemp[] = new String[52];
     int valueD = 0, valueP = 0, i = 0, money = 3000, currentBet;
-    boolean loopChoice = true, replay = true, flag = false, betCheck = false;
+    boolean loopChoice = true, replay = true, flag = false, betCheck = false, checkB = true;
 
     public DeckOfCards() throws InterruptedException {
         for (int x = 0; x < rank.length; x++) {
@@ -28,20 +28,26 @@ public class DeckOfCards {
             }
         }
 
-        shuffle();
-        
         for (int x = 0; x < 52; x++) {
             // System.out.println(deckTemp[x]);
         }
         JOptionPane.showMessageDialog(null, "Welcome to BlackJack!\nYour starting value is $3000.");
 
-        playgame();
-
         do {
+            
             shuffle();
             playgame();
-        } while (replay);
 
+            int choiceR = JOptionPane.showOptionDialog(null, "Would you like to keep playing.", "Replay",
+                    JOptionPane.INFORMATION_MESSAGE, 1, null, buttonRP, buttonRP[0]);
+            
+            if (choiceR == JOptionPane.NO_OPTION || money < 0) {
+                replay = false;
+            }
+
+        } while (replay);
+        JOptionPane.showMessageDialog(null, "Thanks for playing!");
+        System.exit(0);
     }
 
     public void shuffle() {
@@ -60,16 +66,17 @@ public class DeckOfCards {
         valueD = 0;
         i = 0;
         betCheck = false;
-        
-        do{
-            if(betCheck){
+        checkB = true;
+
+        do {
+            if (betCheck) {
                 JOptionPane.showMessageDialog(null, "Please bet within the money you have!");
             }
-        betCheck = true;    
-        String currentBetS = JOptionPane.showInputDialog("How much money would you like to bet on the upcoming hand?\nOnly use numbers! You have $" + money);
-        currentBet = Integer.parseInt(currentBetS);
+            betCheck = true;
+            String currentBetS = JOptionPane.showInputDialog("How much money would you like to bet on the upcoming hand?\nOnly use numbers! You have $" + money);
+            currentBet = Integer.parseInt(currentBetS);
         } while (currentBet > money);
-        
+
         drawP();
         drawD();
         drawP();
@@ -80,32 +87,38 @@ public class DeckOfCards {
             int choice = JOptionPane.showOptionDialog(null, "Would you like to Hit or Stand.", "Play choice.",
                     JOptionPane.INFORMATION_MESSAGE, 1, null, buttons, buttons[0]);
 
-            checkWL(1);
-
             if (choice == JOptionPane.OK_OPTION) {
                 drawP();
                 System.out.println("You are now at a value of " + valueP + ".");
 
                 checkWL(1);
-
             } else if (choice == JOptionPane.NO_OPTION) {
                 loopChoice = false;
+
             } else {
                 System.exit(0);
             }
+
         } while (loopChoice);
 
-        Thread.sleep(500);
-        drawD();
-        System.out.println("The dealer is now at a value of " + valueD + ".");
-
-        while (valueD < 17) {
+        while (checkB) {
+            Thread.sleep(500);
             drawD();
             System.out.println("The dealer is now at a value of " + valueD + ".");
-            checkWL(1);
+
+            while (valueD < 17) {
+                drawD();
+                System.out.println("The dealer is now at a value of " + valueD + ".");
+                checkWL(1);
+                if (checkB = false) {
+                    break;
+                }
+            }
+            while (checkB) {
+                Thread.sleep(2000);
+                checkWL(2);
+            }
         }
-        Thread.sleep(2000);
-        checkWL(2);
     }
 
     public void checkWL(int turn) {
@@ -113,11 +126,14 @@ public class DeckOfCards {
             if (valueP > 21) {
                 money = money - currentBet;
                 System.out.println("You have bust, by drawing over 21. \nYou lose the money you bet on this hand.\n$" + money + " is your new balance.");
-                System.out.println("-----------==============-----------");
+                System.out.println("---------=============---------");
+                loopChoice = false;
+                checkB = false;
             } else if (valueD > 21) {
                 money = money + currentBet;
                 System.out.println("The dealer bust, by drawing over 21. \nYou win the money you bet on this hand.\n$" + money + " is your new balance.");
-                System.out.println("-----------==============-----------");
+                System.out.println("---------=============---------");
+                checkB = false;
             }
         }
 
@@ -125,16 +141,19 @@ public class DeckOfCards {
             if (valueP > valueD) {
                 money = money + currentBet;
                 System.out.println("You won by having more then the dealer. \nYou win the money you bet on this hand.\n$" + money + " is your new balance.");
-                System.out.println("-----------==============-----------");
+                System.out.println("---------=============---------");
+                checkB = false;
             } else if (valueP < valueD) {
                 money = money - currentBet;
                 System.out.println("You lost by having less then the dealer. \nYou lose the money you bet on this hand.\n$" + money + " is your new balance.");
-                System.out.println("-----------==============-----------");
-            } else if (valueP == valueD){
-            System.out.println("Push. Your hand has tied, no exchange of money happens.\n$" + money + " remains your balance.");
-            System.out.println("-----------==============-----------");
-        }
-            
+                System.out.println("---------=============---------");
+                checkB = false;
+            } else if (valueP == valueD) {
+                System.out.println("Push. Your hand has tied, no exchange of money happens.\n$" + money + " remains your balance.");
+                System.out.println("---------=============---------");
+                checkB = false;
+            }
+
         }
     }
 
