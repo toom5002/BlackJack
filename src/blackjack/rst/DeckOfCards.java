9d49c84a25e9668d9d5365420f17e41162bd81d4
toom5002 <45435322+toom5002@users.noreply.github.com>
2019-01-22 13:56:03 -0500

    Finished product with comments
    Finished RST

--- src/blackjack/rst/DeckOfCards.java
+++ src/blackjack/rst/DeckOfCards.java
@@ -8,6 +8,7 @@ public class DeckOfCards {
 
     public static JFrame frame = new JFrame();
     public static JLabel valuePlbl = new JLabel();
+    public static JLabel valueDlbl = new JLabel();
 
     String[] buttonRP = {"Keep playing", "Cash Out"};
     String[] buttons = {"Hit", "Stand"};
@@ -15,7 +16,7 @@ public class DeckOfCards {
     String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
     String deck[][] = new String[13][4];
     String deckTemp[] = new String[52];
-    int valueD = 0, valueP = 0, i = 0, money = 3000, currentBet;
+    int valueD = 0, valueP = 0, i = 0, money = 3000, currentBet, vPx = 587;
     boolean loopChoice = true, replay = true, flag = false, betCheck = false, checkB = true;
 
     public DeckOfCards() throws InterruptedException {
@@ -31,24 +32,29 @@ public class DeckOfCards {
         }
 
         graphics();
-//
-//        JOptionPane.showMessageDialog(null, "Welcome to BlackJack!\nYour starting value is $3000.");
-//
-//        do {
-//
-//            shuffle();
-//            playgame();
-//
-//            int choiceR = JOptionPane.showOptionDialog(null, "Would you like to keep playing.", "Replay",
-//                    JOptionPane.INFORMATION_MESSAGE, 1, null, buttonRP, buttonRP[0]);
-//
-//            if (choiceR == JOptionPane.NO_OPTION || money < 0) {
-//                replay = false;
-//            }
-//
-//        } while (replay);
-//        JOptionPane.showMessageDialog(null, "Thanks for playing!");
-//        System.exit(0);
+
+        JOptionPane.showMessageDialog(null, "Welcome to BlackJack!\nYour starting value is $3000.");
+
+        do {
+
+            shuffle();
+            playgame();
+
+            
+            if (money > 0){
+            int choiceR = JOptionPane.showOptionDialog(null, "Would you like to keep playing.", "Replay",
+                    JOptionPane.INFORMATION_MESSAGE, 1, null, buttonRP, buttonRP[0]);
+            
+            if (choiceR == JOptionPane.NO_OPTION) {
+                replay = false;
+            }
+            } else {
+               JOptionPane.showMessageDialog(null, "Unfortunately you have ran out of money!\nThanks for playing.");
+               System.exit(0);
+            }
+        } while (replay);
+        JOptionPane.showMessageDialog(null, "Thanks for playing!");
+        System.exit(0);
     }
 
     public void graphics() {
@@ -62,20 +68,22 @@ public class DeckOfCards {
         
         JLabel background = new JLabel();
         frame.add(valuePlbl);
+        frame.add(valueDlbl);
         frame.add(background);
 
-        background.setBounds(0, 0, 1200, 750);
+        background.setBounds(0, -23, 1200, 750);
+        valueDlbl.setForeground(Color.WHITE);
+        valueDlbl.setFont(new Font("Serif", Font.PLAIN, 75));
+        valueDlbl.setBounds( 587, 100, 100, 100);
         
         valuePlbl.setForeground(Color.WHITE);
-
-        valuePlbl.setBounds( 600, 525, 10, 10);
-        
+        valuePlbl.setFont(new Font("Serif", Font.PLAIN, 75));
+        valuePlbl.setBounds( vPx, 470, 100, 100);
         
         background.setIcon(new ImageIcon("images/blackjackTable.jpg"));
         valuePlbl.setText(Integer.toString(valueP));
-
+        valueDlbl.setText(Integer.toString(valueD));
         
-
     }
 
     public void shuffle() {
@@ -91,10 +99,15 @@ public class DeckOfCards {
     private void playgame() throws InterruptedException {
         int choice = 0;
         valueP = 0;
+        valuePlbl.setText(Integer.toString(valueP));
         valueD = 0;
-        i = 0;
+        valueDlbl.setText(Integer.toString(valueD));
+        i = 0; 
+        vPx = 587;
+        valuePlbl.setBounds( vPx, 470, 100, 100);
         betCheck = false;
         checkB = true;
+        loopChoice = true;
 
         do {
             if (betCheck) {
@@ -141,6 +154,7 @@ public class DeckOfCards {
             System.out.println("The dealer is now at a value of " + valueD + ".");
 
             while (valueD < 17) {
+                Thread.sleep(500);
                 drawD();
                 System.out.println("The dealer is now at a value of " + valueD + ".");
                 checkWL(1);
@@ -172,6 +186,7 @@ public class DeckOfCards {
         }
 
         if (turn == 2) {
+            System.out.println("\nlast check\n");
             if (valueP > valueD) {
                 money = money + currentBet;
                 System.out.println("You won by having more then the dealer. \nYou win the money you bet on this hand.\n$" + money + " is your new balance.");
@@ -194,52 +209,52 @@ public class DeckOfCards {
         System.out.println("The dealer drew the " + deckTemp[i] + ".");
         if (deckTemp[i].charAt(0) == '2') {
             valueD = valueD + 2;
-            valuePlbl.setText(Integer.toString(valueP));
+            valueDlbl.setText(Integer.toString(valueD));
             //valuePlbl.setText(null);
 
         } else if (deckTemp[i].charAt(0) == '3') {
             valueD = valueD + 3;
-            valuePlbl.setText(Integer.toString(valueP));
+            valueDlbl.setText(Integer.toString(valueD));
             //valuePlbl.setText(null);
 
         } else if (deckTemp[i].charAt(0) == '4') {
             valueD = valueD + 4;
-            valuePlbl.setText(Integer.toString(valueP));
+            valueDlbl.setText(Integer.toString(valueD));
             //valuePlbl.setText(null);
             
         } else if (deckTemp[i].charAt(0) == '5') {
             valueD = valueD + 5;
-            valuePlbl.setText(Integer.toString(valueP));
+            valueDlbl.setText(Integer.toString(valueD));
             //valuePlbl.setText(null);
             
         } else if (deckTemp[i].charAt(0) == '6') {
             valueD = valueD + 6;
-            valuePlbl.setText(Integer.toString(valueP));
+            valueDlbl.setText(Integer.toString(valueD));
             //valuePlbl.setText(null);
             
         } else if (deckTemp[i].charAt(0) == '7') {
             valueD = valueD + 7;
-            valuePlbl.setText(Integer.toString(valueP));
+            valueDlbl.setText(Integer.toString(valueD));
             //valuePlbl.setText(null);
             
         } else if (deckTemp[i].charAt(0) == '8') {
             valueD = valueD + 8;
-            valuePlbl.setText(Integer.toString(valueP));
+            valueDlbl.setText(Integer.toString(valueD));
             //valuePlbl.setText(null);
             
         } else if (deckTemp[i].charAt(0) == '9') {
             valueD = valueD + 9;
-            valuePlbl.setText(Integer.toString(valueP));
+            valueDlbl.setText(Integer.toString(valueD));
            // valuePlbl.setText(null);
             
         } else if (deckTemp[i].charAt(0) == 'A') {
             valueD = valueD + 11;
-            valuePlbl.setText(Integer.toString(valueP));
+            valueDlbl.setText(Integer.toString(valueD));
            // valuePlbl.setText(null);
             
         } else {
             valueD = valueD + 10;
-            valuePlbl.setText(Integer.toString(valueP));
+            valueDlbl.setText(Integer.toString(valueD));
            // valuePlbl.setText(null);
             
         }
@@ -252,34 +267,47 @@ public class DeckOfCards {
         System.out.println("You drew the " + deckTemp[i] + ".");
         if (deckTemp[i].charAt(0) == '2') {
             valueP = valueP + 2;
+            valuePlbl.setText(Integer.toString(valueP));
 
         } else if (deckTemp[i].charAt(0) == '3') {
             valueP = valueP + 3;
+            valuePlbl.setText(Integer.toString(valueP));
 
         } else if (deckTemp[i].charAt(0) == '4') {
             valueP = valueP + 4;
+            valuePlbl.setText(Integer.toString(valueP));
 
         } else if (deckTemp[i].charAt(0) == '5') {
             valueP = valueP + 5;
+            valuePlbl.setText(Integer.toString(valueP));
 
         } else if (deckTemp[i].charAt(0) == '6') {
             valueP = valueP + 6;
+            valuePlbl.setText(Integer.toString(valueP));
 
         } else if (deckTemp[i].charAt(0) == '7') {
             valueP = valueP + 7;
+            valuePlbl.setText(Integer.toString(valueP));
 
         } else if (deckTemp[i].charAt(0) == '8') {
             valueP = valueP + 8;
+            valuePlbl.setText(Integer.toString(valueP));
 
         } else if (deckTemp[i].charAt(0) == '9') {
             valueP = valueP + 9;
+            valuePlbl.setText(Integer.toString(valueP));
 
         } else if (deckTemp[i].charAt(0) == 'A') {
             valueP = valueP + 11;
+            valuePlbl.setText(Integer.toString(valueP));
         } else {
             valueP = valueP + 10;
+            valuePlbl.setText(Integer.toString(valueP));
         }
-
+        if (valueP > 9){
+            vPx = 565;
+            valuePlbl.setBounds( vPx, 470, 100, 100);
+        }
         //System.out.println("You are now at a value of " + valueP + ".");
         i++;
     }
